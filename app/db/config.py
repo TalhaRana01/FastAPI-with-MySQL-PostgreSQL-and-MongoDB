e41aeffe2ca8c8d99674c499aa8a63b6a61ae296
Talha Rana <mtalharana093@gmail.com>
2025-12-13 02:09:21 +0500

    postgress database setup

--- app/db/config.py
+++ app/db/config.py
@@ -4,3 +4,34 @@ from fastapi import Depends
 from typing import AsyncGenerator, Annotated
 from sqlalchemy.ext.asyncio import  AsyncAttrs
 from decouple import config
+
+
+
+DB_USER = config("DB_USER")
+DB_PASS = config("DB_PASS")
+DB_NAME = config("DB_NAME")
+DB_HOST = config("DB_PORT", cast=int)
+
+DATABASE_URL = f"postgressql+asyncpg://{DB_USER}:{DB_PASS}@localhost:{DB_HOST}/{DB_NAME}"
+
+
+
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+async_session = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+  
+
+async def create_tables():
+  async with engine.begin() as conn:
+      await conn.run_sync(Base.metadata.create_all)
+  print("Tables created successfully.")
+  
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
+        
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
+        
+
